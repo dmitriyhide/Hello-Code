2fcdddb76b24321929b8ed20248370f1b40118fd
Dmitriy <dima_product@.com>
2023-07-20 20:37:52 +0300

    Задачи третьего дз

---
+++ zad11/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 25: НАпишите цикл, который принимает на вход 
+//два числа (A и B) и возводит A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Method(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+int method = Method(numberA, numberB);
+Console.WriteLine("Ответ: " + method);
